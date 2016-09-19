325c33719943b2e609f80b4393080e69c8162df1
Perkovec <perkovec24@gmail.com>
2016-09-19 10:30:55 +0300

    Add ESLint

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,9 @@
 const Router = require('node-router');
 const Static = require('./static');
-const path = require('path');
 
 class Routes {
   constructor(config) {
-    this.router = Router();
+    this.router = new Router();
     this.static = new Static(config.root);
   }
 
@@ -12,12 +11,11 @@ class Routes {
     const route = this.router.push;
     const www = this.static;
 
-    route('/', (req, res, next) => {
+    route('/', (req, res) => {
       www.getFile('index.html', true).then(
         data => res.send(data),
         err => res.send(404, err)
       );
-      
     });
   }
 }
